58abe78ba9c70709e61600e0a30395a1f449076a
Marcus Johansson <marcusjohansson0111@gmail.com>
2022-09-20 11:25:22 +0200

    Splash Screen has been created

---
+++ Dropspot/ViewControllers/SplashScreenController.swift
@@ -0,0 +1,57 @@
+//
+//  SplashScreenController.swift
+//  Dropspot
+//
+//  Created by Marcus Johansson on 2022-09-20.
+//
+
+import UIKit
+import AVFoundation
+
+class SplashScreenController: UIViewController {
+    @IBOutlet weak var SplashLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        splashaction()
+    }
+    
+    
+    @objc
+    func splashaction() {
+        SplashLabel.text! += "D"
+        sound()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            self.SplashLabel.text! += "r"
+            self.sound()
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                self.SplashLabel.text! += "o"
+                self.sound()
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                    self.SplashLabel.text! += "p"
+                    self.sound()
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                        self.SplashLabel.text! += "s"
+                        self.sound()
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                            self.SplashLabel.text! += "p"
+                            self.sound()
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                                self.SplashLabel.text! += "o"
+                                self.sound()
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                                    self.SplashLabel.text! += "t"
+                                    self.sound()
+                                    DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    func sound(){
+        AudioServicesPlaySystemSound(1306)
+    }
+}
