c4ca5816cce979860c3d8ef6f13cd6f6c46001de
Vinc93 <91186316+Vinc93@users.noreply.github.com>
2022-10-13 10:23:35 +0200

    Squashed commit of the following:
    commit 3cf69942a6c363c9a894f3b2630d3971531318f3
    Author: Vinc93 <91186316+Vinc93@users.noreply.github.com>
    Date:   Thu Oct 13 00:44:31 2022 +0200
    
        Upp 4
    
    commit 2cb68aebadf9557b9387b431a088775f8d2e0fcc
    Author: Vinc93 <91186316+Vinc93@users.noreply.github.com>
    Date:   Tue Oct 11 12:44:45 2022 +0200
    
        upp 3
    
    commit df807cb1670155d4145c22cb11e98a0b19960e1d
    Author: Vinc93 <91186316+Vinc93@users.noreply.github.com>
    Date:   Tue Oct 11 12:15:59 2022 +0200
    
        upp2

---
+++ Dropspot/ViewControllers/MyCompanyViewController.swift
@@ -0,0 +1,70 @@
+//
+//  MyCompanyViewController.swift
+//  Dropspot
+//
+//  Created by Vincent Pilström Tipmung on 2022-10-03.
+//
+
+
+
+import UIKit
+
+struct company{
+    var name:String
+    var adress:String
+    var followers:Int
+    var image:String
+    var phone:String
+    var eMail:String
+    var orgNr:String
+    init(name: String, adress: String, followers: Int, image: String, phone:String,eMail:String,orgNr:String) {
+        self.name = name
+        self.adress = adress
+        self.followers = followers
+        self.image = image
+        self.phone = phone
+        self.eMail = eMail
+        self.orgNr = orgNr
+    }
+}
+
+class MyCompanyViewController:UIViewController{
+    
+    @IBOutlet weak var CompTitle: UILabel!
+    
+    @IBOutlet weak var CompAdress: UILabel!
+    
+    
+    @IBOutlet weak var CompPhone: UILabel!
+    
+    @IBOutlet weak var CompEMail: UILabel!
+    
+    @IBOutlet weak var CompOrg: UILabel!
+    
+    @IBOutlet weak var CompImage: UIImageView!
+    
+    @IBOutlet weak var CompFollowers: UILabel!
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    let companies = [company(name: "H&M", adress: "Plöjargatan 7, Göteborg", followers: 137, image: "H&M-Logo.svg",phone:"0534-61816",eMail: "info@hm.se",orgNr: "771122-1234")]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        CompTitle.text="\(companies[0].name)";
+        CompAdress.text="Address: \(companies[0].adress)";
+        CompPhone.text="Phone Number: \(companies[0].phone)";
+        CompEMail.text="Email: \(companies[0].eMail)";
+        CompOrg.text="Organization number: \(companies[0].orgNr)";
+        CompFollowers.text="Followers: \(companies[0].followers)";
+        CompImage.image = UIImage(named: "\(companies[0].image)");
+        
+    }
+    
+}
