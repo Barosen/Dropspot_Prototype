23678405ae9b089793770809537c654cef77ce12
kevinzett <kevin_zetterlind@outlook.com>
2022-10-27 09:42:58 +0200

    commit

---
+++ Dropspot/ViewControllers/PaymentViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PaymentViewController.swift
+//  Dropspot
+//
+//  Created by Kevin Zetterlind on 2022-10-27.
+//
+
+import UIKit
+
+class PaymentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
