fee0841270c788ccb47cca5ed66881280ea2d530
alizaeri <90989935+alizaeri@users.noreply.github.com>
2022-10-27 15:15:58 +0200

    Squashed commit of the following:
    commit aa5750a030eb4bfd4a6dfc253f8e31ab4f4d9ef8
    Author: alizaeri <90989935+alizaeri@users.noreply.github.com>
    Date:   Thu Oct 27 15:13:03 2022 +0200
    
        favorit2
    
    commit 4f8c1c1b7793c31b981a5944d9d611ab5eef7a22
    Author: alizaeri <90989935+alizaeri@users.noreply.github.com>
    Date:   Thu Oct 27 14:40:08 2022 +0200
    
        favorite
    
    commit 9f3977f348e03bfb16ff736d534a7b8f62ad17b5
    Author: alizaeri <90989935+alizaeri@users.noreply.github.com>
    Date:   Mon Oct 24 13:56:03 2022 +0200
    
        step3
    
    commit af2a2dcf26fa11f44e1d40075c036ec8b7d21407
    Author: alizaeri <90989935+alizaeri@users.noreply.github.com>
    Date:   Mon Oct 24 13:09:38 2022 +0200
    
        step2
    
    commit a4181cccf6b15f2fbf1a82bdf296f6d64123690a
    Author: alizaeri <90989935+alizaeri@users.noreply.github.com>
    Date:   Mon Oct 24 12:36:27 2022 +0200
    
        CompanyProfile

---
+++ Dropspot/ViewControllers/CompanyProfile.swift
@@ -0,0 +1,19 @@
+//
+//  CompanyProfile.swift
+//  Dropspot
+//
+//  Created by Ali Zaery on 2022-10-24.
+//
+import UIKit
+
+class CompanyProfile: UIViewController {
+    
+   
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+    
+}
--- Dropspot/ViewControllers/FavoritesViewController.swift
+++ Dropspot/ViewControllers/FavoritesViewController.swift
@@ -9,28 +9,49 @@ import UIKit
 
 class FavoritesViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
     @IBOutlet weak var tableViewFavorit: UITableView!
-    let favoritAnnonces = [
-        Annonce(title: "flower", image: "image.jpg"),
-        Annonce(title: "kläder", image: "image1"),
-        Annonce(title: "Food", image: "McDonalds"),
-        Annonce(title: "rabat", image: "discount")
-        
-    ]
+    
+    
+    
+    
+   
+    let list2 = MainList.annonces.filter{annonse in
+        return (annonse.favorite == true)}
+    
+ 
+    
+    //var list2 : [Annonce] =
+    
+    
+    
+
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        favoritAnnonces.count
+        list2.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         tableView.isPagingEnabled = true
         let cell = tableView.dequeueReusableCell(withIdentifier: "tableCellFavorit", for: indexPath)
         as! tableViewFavoritCell
-        let annonce = self.favoritAnnonces[indexPath.row]
+        let annonce = self.list2[indexPath.row]
         cell.tableViewFavoritCellBackgrund.image = UIImage(named: annonce.image)
         cell.commentFavoritCellBtn.setImage(UIImage(named: "commentButton"), for: UIControl.State())
-        cell.favoritFavoritCellBtn.setImage(UIImage(named: "favoriteButton"), for: UIControl.State())
+       
         cell.shareFavoritCellBtn.setImage(UIImage(named: "shareButton"), for: UIControl.State())
         cell.profileFavoritCellBtn.setImage(UIImage(named: "followButton"), for: UIControl.State())
+        let image = UIImage(named: "favoriteOn")
+        let image2 = UIImage(named: "favorite5")
+        if (annonce.favorite){
+            
+           cell.favoritFavoritCellBtn.setImage(image, for: UIControl.State.normal)
+            //cell.favoriteButton.imageEdgeInsets = UIEdgeInsets(top: 30, left: 30, bottom: 30, right: 30)
+
+           // cell.favoriteButton.contentMode = UIView.ContentMode.center
+            
+        } else {
+            cell.favoritFavoritCellBtn.setImage(image2, for: UIControl.State.normal)
+           // cell.favoriteButton.contentMode = UIView.ContentMode.center
+        }
         
         
       
